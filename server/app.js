1822c4036391d64973ec5b142296fde1d4bfe428
Sean Harrell <seanvharrell@gmail.com>
2020-03-07 19:33:54 +0000

    data file working

--- server/app.js
+++ server/app.js
@@ -3,24 +3,26 @@
 const express = require('express');
 const morgan = require('morgan');
 const http = require('http');
-const data = require('../server/data.json');
+const data = require('./data.json');
 
 
 // create your express server below
-var app = express();
-
+const app = express();
 
 
 // add your routes and middleware below
+
+
+app.use(express.static('server'));
+
 app.get('/', function(req, res){
   res.status(200).send('its working');
 });
 
-app.get('/data'), function (req , res){
-
-
-}
-
+// The "/data" is needed in localhost url in order to see the data
+app.get('/data',function(req, res){
+  res.status(200).send(data);
+});
 
 
 // finally export the express application
