8c3bb174e91c3849c48ccb29176b73f6e3f18e2a
Sean Harrell <seanvharrell@gmail.com>
2020-03-07 03:12:54 +0000

    created server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,28 @@
 // import files and packages up here
 
+const express = require('express');
+const morgan = require('morgan');
+const http = require('http');
+const data = require('../server/data.json');
+
 
 // create your express server below
-var app;
+var app = express();
+
+
 
 // add your routes and middleware below
+app.get('/', function(req, res){
+  res.status(200).send('its working');
+});
+
+app.get('/data'), function (req , res){
+
+
+}
+
+
 
 // finally export the express application
+
 module.exports = app;
