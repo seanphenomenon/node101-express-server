176ceaca6d3e23d9afe7ce312ce22e0697dc6b5a
Sean Harrell <seanvharrell@gmail.com>
2020-03-25 09:09:09 +0000

    server edit

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 // here we're importing your express app
 const server = require('./app');
 
-// using port for environmental access to deploy to heroku
 const PORT = process.env.PORT || 3000;
 
-// We're telling the app to listen for network requests on port 3000
 server.listen(PORT, function() {
   console.log(`Server is listening on http://localhost:${PORT}`);
 });
