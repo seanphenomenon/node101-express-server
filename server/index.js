994458d6297c82f4dade8d08c5387614780b1fb8
Sean Harrell <seanvharrell@gmail.com>
2020-03-25 06:36:43 +0000

    reupdated

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const server = require('./app');
 const PORT = process.env.PORT || 3000;
 
 // We're telling the app to listen for network requests on port 3000
-server.listen(3000, function() {
+server.listen(PORT, function() {
   console.log(`Server is listening on http://localhost:${PORT}`);
 });
 
