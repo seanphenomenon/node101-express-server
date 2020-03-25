cc0999e78a17c092a2fb58e4ef825ec24abbe460
Sean Harrell <seanvharrell@gmail.com>
2020-03-25 07:33:02 +0000

    comment added

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 // here we're importing your express app
 const server = require('./app');
 
-
+// using port for environmental access
 const PORT = process.env.PORT || 3000;
 
 // We're telling the app to listen for network requests on port 3000
